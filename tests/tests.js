976b71ce459f01ad385db10de9a31ecf24f35715
Dave Taylor <davegss@gmail.com>
2015-01-15 17:59:18 +0000

    First commit of baseline code

---
+++ tests/tests.js
@@ -0,0 +1,11 @@
+var should = require('should');
+var assert = require('assert');
+
+describe('Test Framework', function () {
+    it('should have mocha installed and running.', function () {
+        assert.equal(true, true);
+    })
+    it('should have the should library installed and running.', function () {
+        true.should.eql(true);
+    })
+});
